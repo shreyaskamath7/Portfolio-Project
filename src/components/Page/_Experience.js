6d8a75395531db4108c290d81292e92ccce86d46
shreyaskamath7 <shreyaskamath7781@gmail.com>
2021-04-25 14:56:55 +0530

    changes

--- src/components/Page/_Experience.js
+++ src/components/Page/_Experience.js
@@ -33,7 +33,7 @@ const Experience = () => {
                     </div>
                     <div className="d-flex flex-column flex-md-row justify-content-between">
                         <div className="flex-grow-1">
-                            <h3 className="mb-0">Web Design Intern</h3>
+                            <h3 className="mb-0">Web Design ern</h3>
                             <div className="subheading mb-3">Shout! Media Productions</div>
                             <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
                         </div>
